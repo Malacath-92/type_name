1b73fb63068f32bf026dac51e7c05c6bc72be310
Hanno <vingine.dev@gmail.com>
2023-11-22 20:48:12 +0100

    Actually fix includes

--- include/type_name/type_name.gcc.hpp
+++ include/type_name/type_name.gcc.hpp
@@ -2,9 +2,9 @@
 #define ADAM_BADURA_TYPE_NAME_TYPE_NAME_GCC_HPP
 
 #if (__GNUC__ > 8) || (__GNUC__ == 8 && __GNUC_MINOR__ >= 1)
-#	include "type_name.gcc.8.1.hpp"
+#	include "type_name.gcc.8_1.hpp"
 #elif (__GNUC__ > 7) || (__GNUC__ == 7 && __GNUC_MINOR__ >= 3)
-#	include "type_name.gcc.7.3.hpp"
+#	include "type_name.gcc.7_3.hpp"
 #else
 #	error When using GCC, at least version 7.3 is required.
 #endif
